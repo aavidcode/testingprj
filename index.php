7ef150ab49f8591421d233aada7248a24aea48fc
apeksha9july <apeksha9july@gmail.com>
2014-07-28 11:36:17 +0530

    apeksha changes

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-         echo 'laxman';
+         kjghfghhjkl;
+		 echo date('Y-m-d');
         // put your code here
         ?>
     </body>
